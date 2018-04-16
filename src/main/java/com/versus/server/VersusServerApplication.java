786e1712c4d3e1b51bde2cb81f9c8c5d68205071
Yaroslav19 <galaolala@gmail.com>
2018-04-16 22:04:04 +0300

    Initial versus server

---
+++ src/main/java/com/versus/server/VersusServerApplication.java
@@ -0,0 +1,12 @@
+package com.versus.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersusServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VersusServerApplication.class, args);
+	}
+}
